3e9ee622fe50d013dbfd027371a0c6855235f0d3
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-10-25 21:53:16 +0900

    mobx-practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useLocalStore, useObserver } from 'mobx-react'
+
+const StoreContext = React.createContext()
+  
+const StoreProvider = ({ children }) => {
+    
+  const store = useLocalStore(() => ({
+    bugs: ["Centipede"],
+    addBug: (bug) => {
+      store.bugs.push(bug)
+    },
+    get bugsCount(){
+      return store.bugs.length
+    }
+  }))
+
+  return (
+    <StoreContext.Provider value={store}>
+      {children}
+    </StoreContext.Provider>
+  )
+}
+
+const BugsHeader = () => {
+  const store = React.useContext(StoreContext)
+  return useObserver(() => (
+    <h1>{store.bugsCount} Bugs</h1>
+  ))
+}
+
+const BugsList = () => {
+  const store = React.useContext(StoreContext)
+
+  return useObserver(() => (
+    <ul>
+      {store.bugs.map(bug => (
+        <li key={bug}>{bug}</li>
+      ))}
+    </ul>
+  ))
+}
+
+const BugsForm = () => {
+  const store = React.useContext(StoreContext)
+  const [bug, setBug] = React.useState('')
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault()
+        store.addBug(bug)
+        setBug('')
+      }}
+    >
+      <input type='text' value={bug} onChange={e => {setBug(e.target.value)}} />
+      <button>Add</button>
+    </form>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StoreProvider>
+      <main>
+        <BugsHeader />
+        <BugsList />
+        <BugsForm />
+      </main>
+    </StoreProvider>
+  )
 }
 
-export default App;
+export default App
